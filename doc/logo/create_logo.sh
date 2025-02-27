73f362451623efaa222166b3ff7f1bb869644b59
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2025-02-27 13:18:11 +0100

    Add square BW logo

--- doc/logo/create_logo.sh
+++ doc/logo/create_logo.sh
@@ -32,6 +32,7 @@ gs -dFirstPage=1 -dLastPage=1 -sDEVICE=pngalpha -dTextAlphaBits=4 -r2442 -o logo
 gs -dFirstPage=2 -dLastPage=2 -sDEVICE=pngalpha -dTextAlphaBits=4 -r2442 -o logo_color.png logo_icon.pdf
 gs -dFirstPage=3 -dLastPage=3 -sDEVICE=pngalpha -dTextAlphaBits=4 -r2442 -o logo_square.png logo_icon.pdf
 gs -dFirstPage=4 -dLastPage=4 -sDEVICE=pngalpha -dTextAlphaBits=4 -r1250 -o icon_256.png logo_icon.pdf
+gs -dFirstPage=5 -dLastPage=5 -sDEVICE=pngalpha -dTextAlphaBits=4 -r2442 -o logo_square_bw.png logo_icon.pdf
 
 # Create the image placement
 pdflatex item_placement.tex
